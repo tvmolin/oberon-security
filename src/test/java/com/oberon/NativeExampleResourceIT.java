640db78578fa0cc4238945fc8e5b36bdc30be1af
root <root@localhost.localdomain>
2019-10-25 20:09:15 -0300

    Initial commit

---
+++ src/test/java/com/oberon/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.oberon;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
